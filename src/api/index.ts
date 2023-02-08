8fd762353abe67bed2187cfd5ef78c0e98e95a0f
David Mokel <Moqqll.china@gmail.com>
2023-02-08 21:52:41 +0800

    feat(all): init with hapi/fastify, graphql, helix, nexus

---
+++ src/api/index.ts
@@ -0,0 +1,107 @@
+import { schema } from "@/api/schema";
+import Hapi from '@hapi/hapi';
+import fastify from "fastify";
+import { getGraphQLParameters, processRequest, renderGraphiQL, Request, sendResult, shouldRenderGraphiQL } from "graphql-helix";
+import qs from "qs";
+
+// TODO
+// hapi会在【await sendResult(result, req.raw.res)】发生err: Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+// 先不用hapi，等待后续官方反馈
+export const startHapi = async () => {
+  const server = Hapi.server({
+    port: 8777,
+    query: {
+      parser: (query) => qs.parse(query),
+    },
+    routes: {
+      cors: {
+        origin: ["*"]
+      }
+    }
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: () => { return `Http and Graphql Server ${new Date()}`; }
+  })
+
+  server.route({
+    method: ["POST", "GET"],
+    path: "/graphql",
+    handler: async (req, h) => {
+      const request: Request = {
+        headers: req.headers,
+        method: req.method,
+        query: req.query,
+        body: req.payload,
+      };
+
+      // if (shouldRenderGraphiQL(request)) {
+      //   return h.response((
+      //     renderGraphiQL({
+      //       endpoint: "/graphql",
+      //     })
+      //   )).type("text/html");
+      // }
+
+      const { operationName, query, variables } = getGraphQLParameters(request);
+
+      const result = await processRequest({
+        request,
+        schema,
+        operationName,
+        query,
+        variables,
+      });
+
+      await sendResult(result, req.raw.res);
+    }
+  })
+
+  await server.start();
+  console.log(`Serve on ${server.info.uri}`);
+}
+
+export const startFastify = async () => {
+  const server = fastify();
+
+  server.get("/", (_, reply) => { reply.send(`Http and Graphql Server ${new Date()}`) });
+
+  server.route({
+    method: ["POST", "GET"],
+    url: "/graphql",
+    handler: async (req, reply) => {
+      const request: Request = {
+        headers: req.headers,
+        method: req.method,
+        query: req.query,
+        body: req.body,
+      };
+
+      if (shouldRenderGraphiQL(request)) {
+        reply.header("Content-Type", "text/html");
+        reply.send(
+          renderGraphiQL({
+            endpoint: "/graphql",
+          })
+        );
+        return;
+      }
+
+      const { operationName, query, variables } = getGraphQLParameters(request);
+
+      const result = await processRequest({
+        request,
+        schema,
+        operationName,
+        query,
+        variables,
+      });
+
+      await sendResult(result, reply.raw);
+    }
+  });
+
+  server.listen({ port: 8777, host: "0.0.0.0" }, () => { console.log(`Server is running on http://127.0.0.1:8777`); });
+}
---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.join(process.cwd(), 'env', `${process.env.SERVER_ENV ?? ''}.env`) })
+
+import { startFastify } from "@/api/index";
+
+process.on("unhandledRejection", (err) => {
+  console.log(`unhandledRejection, err: ${err}`);
+
+  // For now, just skip any unhandled errors
+  // process.exit(1);
+});
+
+const setup = async () => { };
+
+setup().then(() => startFastify());
